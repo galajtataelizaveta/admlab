b80a0aa2fa51870c10fc1a5c3f7f93eab7b2ed1c
galajtataelizaveta <116155010+galajtataelizaveta@users.noreply.github.com>
2024-02-20 00:36:03 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
 import pytest
 import os.path
-import main
+import lab_1
 
 # Тест на создание БД
 def test_create_database():  
-    main.init_db()
-    assert os.path.exists(main.db_name) == True
+    lab_1.init_db()
+    assert os.path.exists(lab_1.db_name) == True
 
 # Тест на наличие столбцов в Clients
 def test_clients():  
-    assert main.Clients.name == False
-    assert main.Clients.city == False
-    assert main.Clients.address == False
+    assert lab_1.Clients.name == False
+    assert lab_1.Clients.city == False
+    assert lab_1.Clients.address == False
 
 # Тест на наличие столбцов в Orders
 def test_orders():  
-    assert main.Orders.clients == False
-    assert main.Orders.amount == False
-    assert main.Orders.date == False
-    assert main.Orders.description == False
+    assert lab_1.Orders.clients == False
+    assert lab_1.Orders.amount == False
+    assert lab_1.Orders.date == False
+    assert lab_1.Orders.description == False
 
 # Тест на наличие 10 строк в Clients
 def test_sum_clients():  
-    main.fill_db()
-    assert len(main.Clients.select()) > 10
+    lab_1.fill_db()
+    assert len(lab_1.Clients.select()) > 10
 
 # Тест на наличие 10 строк в Orders
 def test_sum_orders():  
-    main.fill_db()
-    assert len(main.Orders.select()) > 10
+    lab_1.fill_db()
+    assert len(lab_1.Orders.select()) > 10
