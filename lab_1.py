49f49d9b5744addf6c55196194b438d219bd6026
galajtataelizaveta <116155010+galajtataelizaveta@users.noreply.github.com>
2024-03-19 00:03:02 +0500

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -1,43 +1,33 @@
 from peewee import *
 
-# Определение имени БД
-database = 'database.db'
+# Создание подключения к БД SQLite
+db = SqliteDatabase('database.db')
 
-# Определение подключения к БД
-db = SqliteDatabase(database)
 # Определение моделей
 class BaseModel(Model):
     class Meta:
         database = db
 
-class Clients(BaseModel):
+class Client(BaseModel):
     name = CharField()
     city = CharField()
     address = CharField()
 
-class Orders(BaseModel):
-    client = ForeignKeyField(Clients, backref='orders')
+class Order(BaseModel):
+    client = ForeignKeyField(Client, backref='orders')
     date = DateField()
     amount = DecimalField(max_digits=10, decimal_places=2)
     description = TextField()
-# Определение функции для очистки таблиц
-def clear_tables():
-        db.connect(reuse_if_open=True)
-        False
-        Clients.delete().execute()
-        Orders.delete().execute()
-        #db.close()
+
 # Функция для создания базы данных
 def initialize_database():
-    db.connect(reuse_if_open=True)
-    False
-    db.create_tables([Clients, Orders])
-    #db.close()
+    db.connect()
+    db.create_tables([Client, Order])
+    db.close()
 
 # Функция для заполнения тестовыми данными
 def fill_database():
-
-    clients_data = [
+  clients_data = [
         {"name": "Rosa McBride", "city": "Brest", "address": "352 Main St"},
         {"name": "Bernice Young", "city": "Krakow", "address": "345 Elm St"},
         {"name": "Bob Johnson", "city": "Mexico", "address": "544 Oak St"},
@@ -50,8 +40,7 @@ def fill_database():
         {"name": "Olga Mills", "city": "Tokyo", "address": "856 Walnut St"}
     ]
 
-
-    orders_data = [
+      orders_data = [
         {"client": 1, "date": "2024-03-01", "amount": 90.80, "description": "Order 1 for Rosa McBride"},
         {"client": 2, "date": "2024-03-02", "amount": 140.75, "description": "Order 1 for Bernice Young"},
         {"client": 3, "date": "2024-03-03", "amount": 2700.25, "description": "Order 1 for Bob Johnson"},
@@ -64,36 +53,33 @@ def fill_database():
         {"client": 10, "date": "2024-03-10", "amount": 155.35, "description": "Order 1 for Olga Mills"}
     ]
 
-    with db:
-        for client_data in clients_data:
-            Clients.create(**client_data)
+    db.connect()
+    for client_data in clients_data:
+        Client.create(**client_data)
 
-        for order_data in orders_data:
-            Orders.create(**order_data)
-    clear_tables()
-
+    for order_data in orders_data:
+        Order.create(**order_data)
+    db.close()
 
 # Функция для отображения содержимого таблицы
 def show_table(table_name):
-    db.connect(reuse_if_open=True)
-    False
+    db.connect()
     if table_name == "CLIENTS":
-        for client in Clients.select():
+        for client in Client.select():
             print(f"{client.name}\t{client.city}\t{client.address}")
     elif table_name == "ORDERS":
-        for order in Orders.select():
+        for order in Order.select():
             print(f"{order.client}\t{order.date}\t{order.amount}\t{order.description}")
-    #db.close()
+    db.close()
 
 import sys
 
 # Распределение действий в зависимости от переданных параметров
 if len(sys.argv) == 1:
     print("Usage:")
-    print(" init -  создает базу данных")
-    print(" fill - заполняет базу данных тестовыми данными")
-    print(" show [table_name] - отображает содержимое указанной таблицы.")
-
+    print(" init - to create the database")
+    print(" fill - to fill the database with test data")
+    print(" show [table_name] - to show the content of the specified table")
 
 elif len(sys.argv) > 1:
     if sys.argv[1] == "init":
@@ -104,4 +90,4 @@ elif len(sys.argv) > 1:
         if len(sys.argv) == 3:
             show_table(sys.argv[2])
         else:
-            print("Please provide table name to show its contents")
+            print("Please provide table name to show its contents.")
