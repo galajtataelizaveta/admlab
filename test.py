c744ff9d746db025df75368ec2f55597e252c479
galajtataelizaveta <116155010+galajtataelizaveta@users.noreply.github.com>
2024-03-19 00:03:30 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,50 +1,31 @@
 import pytest
 import os.path
 import lab_1
-from lab_1 import clear_tables, fill_database, Clients, Orders, db
-
-# Фикстура для проверки отсутствия базы данных
-@pytest.fixture(autouse=True)
-def check_database_exists():
-    if not os.path.exists(lab_1.database):
-        pytest.fail("Не существует базы данных")
 
 # Тест на создание БД
-def test_create_database():
-    lab_1.initialize_database()
-    assert os.path.exists(lab_1.database) == True
+def test_create_database():  
+    lab_1.init_db()
+    assert os.path.exists(lab_1.db_name) == True
 
 # Тест на наличие столбцов в Clients
-def test_clients():
-    assert hasattr(lab_1.Clients, "name")
-    assert hasattr(lab_1.Clients, "city")
-    assert hasattr(lab_1.Clients, "address")
+def test_clients():  
+    assert lab_1.Clients.name == False
+    assert lab_1.Clients.city == False
+    assert lab_1.Clients.address == False
 
 # Тест на наличие столбцов в Orders
-def test_orders():
-    assert hasattr(lab_1.Orders, "client")
-    assert hasattr(lab_1.Orders, "amount")
-    assert hasattr(lab_1.Orders, "date")
-    assert hasattr(lab_1.Orders, "description")
+def test_orders():  
+    assert lab_1.Orders.clients == False
+    assert lab_1.Orders.amount == False
+    assert lab_1.Orders.date == False
+    assert lab_1.Orders.description == False
 
-# Тест на проверку количества клиентов в базе данных
-def test_sum_clients():
-    with pytest.raises(Exception):
-        with db:
-            clear_tables()
-            fill_database()
+# Тест на наличие 10 строк в Clients
+def test_sum_clients():  
+    lab_1.fill_db()
+    assert len(lab_1.Clients.select()) > 10
 
-            num_clients = Clients.select().count()
-            assert num_clients >= 0
-
-# Тест на проверку количества заказов в базе данных
-def test_sum_orders():
-    with pytest.raises(Exception):
-        with db:
-            clear_tables()
-            fill_database()
-
-            num_orders = Orders.select().count()
-            assert num_orders >= 0
-
-
+# Тест на наличие 10 строк в Orders
+def test_sum_orders():  
+    lab_1.fill_db()
+    assert len(lab_1.Orders.select()) > 10
