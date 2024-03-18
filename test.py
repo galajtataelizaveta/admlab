3ece085fb92a05a30715974a8c9219fcae04235b
galajtataelizaveta <116155010+galajtataelizaveta@users.noreply.github.com>
2024-03-19 00:17:04 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,50 +1,36 @@
-import pytest
-import os.path
-import lab_1
-from lab_1 import clear_tables, fill_database, Clients, Orders, db
+from lab_1 import init, fill, show, db, ORDERS, CLIENTS
+from peewee import *
+from os.path import exists
 
-# Фикстура для проверки отсутствия базы данных
-@pytest.fixture(autouse=True)
-def check_database_exists():
-    if not os.path.exists(lab_1.database):
-        pytest.fail("Не существует базы данных")
+def test_init():
+    db.connect()
+    init()
+    assert exists("database.db")
+    db.close()
 
-# Тест на создание БД
-def test_create_database():
-    lab_1.initialize_database()
-    assert os.path.exists(lab_1.database) == True
+def test_init_second_time():
+    db.connect()
+    init()
+    assert exists(database.db")
+    db.close()
 
-# Тест на наличие столбцов в Clients
-def test_clients():
-    assert hasattr(lab_1.Clients, "name")
-    assert hasattr(lab_1.Clients, "city")
-    assert hasattr(lab_1.Clients, "address")
+def test_show_empty():
+    db.connect()
+    for tablename in ("clients", "orders"):
+        show(tablename)
+    db.close()
 
-# Тест на наличие столбцов в Orders
-def test_orders():
-    assert hasattr(lab_1.Orders, "client")
-    assert hasattr(lab_1.Orders, "amount")
-    assert hasattr(lab_1.Orders, "date")
-    assert hasattr(lab_1.Orders, "description")
+def test_fill():
+    db.connect()
+    for i in range(25):
+        fill()
+    assert CLIENTS.select().count() >= 10
+    db.close()
 
-# Тест на проверку количества клиентов в базе данных
-def test_sum_clients():
-    with pytest.raises(Exception):
-        with db:
-            clear_tables()
-            fill_database()
-
-            num_clients = Clients.select().count()
-            assert num_clients >= 0
-
-# Тест на проверку количества заказов в базе данных
-def test_sum_orders():
-    with pytest.raises(Exception):
-        with db:
-            clear_tables()
-            fill_database()
-
-            num_orders = Orders.select().count()
-            assert num_orders >= 0
+def test_show():
+    db.connect()
+    for tablename in ("clients", "orders"):
+        show(tablename)
+    db.close()
 
 
