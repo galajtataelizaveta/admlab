d5afdfe91c5c5056c3d9bff79ff9d3cf350b1a94
galajtataelizaveta <116155010+galajtataelizaveta@users.noreply.github.com>
2024-03-19 00:14:22 +0500

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -1,43 +1,31 @@
-from peewee import *
+from peewee import *    
+import argparse
 
-# Определение имени БД
-database = 'database.db'
+db = SqliteDatabase("база1.db")
 
-# Определение подключения к БД
-db = SqliteDatabase(database)
-# Определение моделей
-class BaseModel(Model):
+
+class CLIENTS(Model):
+    NAME = CharField()
+    CITY = CharField()
+    ADDRESS = CharField()
+
     class Meta:
         database = db
+        table_name = "Clients"
 
-class Clients(BaseModel):
-    name = CharField()
-    city = CharField()
-    address = CharField()
 
-class Orders(BaseModel):
-    client = ForeignKeyField(Clients, backref='orders')
-    date = DateField()
-    amount = DecimalField(max_digits=10, decimal_places=2)
-    description = TextField()
-# Определение функции для очистки таблиц
-def clear_tables():
-        db.connect(reuse_if_open=True)
-        False
-        Clients.delete().execute()
-        Orders.delete().execute()
-        #db.close()
-# Функция для создания базы данных
-def initialize_database():
-    db.connect(reuse_if_open=True)
-    False
-    db.create_tables([Clients, Orders])
-    #db.close()
+class ORDERS(Model):
+    CLIENT = ForeignKeyField(CLIENTS)
+    DATE = DateField()
+    AMOUNT = IntegerField()
+    DESCRIPTION = CharField()
 
-# Функция для заполнения тестовыми данными
-def fill_database():
+    class Meta:
+        database = db
+        table_name = "Orders"
 
-    clients_data = [
+
+clients_data =   clients_data = [
         {"name": "Rosa McBride", "city": "Brest", "address": "352 Main St"},
         {"name": "Bernice Young", "city": "Krakow", "address": "345 Elm St"},
         {"name": "Bob Johnson", "city": "Mexico", "address": "544 Oak St"},
@@ -50,8 +38,7 @@ def fill_database():
         {"name": "Olga Mills", "city": "Tokyo", "address": "856 Walnut St"}
     ]
 
-
-    orders_data = [
+orders_data = [
         {"client": 1, "date": "2024-03-01", "amount": 90.80, "description": "Order 1 for Rosa McBride"},
         {"client": 2, "date": "2024-03-02", "amount": 140.75, "description": "Order 1 for Bernice Young"},
         {"client": 3, "date": "2024-03-03", "amount": 2700.25, "description": "Order 1 for Bob Johnson"},
@@ -64,44 +51,60 @@ def fill_database():
         {"client": 10, "date": "2024-03-10", "amount": 155.35, "description": "Order 1 for Olga Mills"}
     ]
 
-    with db:
-        for client_data in clients_data:
-            Clients.create(**client_data)
 
-        for order_data in orders_data:
-            Orders.create(**order_data)
-    clear_tables()
-
-
-# Функция для отображения содержимого таблицы
-def show_table(table_name):
-    db.connect(reuse_if_open=True)
-    False
-    if table_name == "CLIENTS":
-        for client in Clients.select():
-            print(f"{client.name}\t{client.city}\t{client.address}")
-    elif table_name == "ORDERS":
-        for order in Orders.select():
-            print(f"{order.client}\t{order.date}\t{order.amount}\t{order.description}")
-    #db.close()
-
-import sys
-
-# Распределение действий в зависимости от переданных параметров
-if len(sys.argv) == 1:
-    print("Usage:")
-    print(" init -  создает базу данных")
-    print(" fill - заполняет базу данных тестовыми данными")
-    print(" show [table_name] - отображает содержимое указанной таблицы.")
-
-
-elif len(sys.argv) > 1:
-    if sys.argv[1] == "init":
-        initialize_database()
-    elif sys.argv[1] == "fill":
-        fill_database()
-    elif sys.argv[1] == "show":
-        if len(sys.argv) == 3:
-            show_table(sys.argv[2])
+def pretty_print(*args, FIRST=False):
+    MAX = 12
+    data = []
+    for arg in args:
+        if len(arg) > MAX:
+            data.append(arg[:MAX])
         else:
-            print("Please provide table name to show its contents.")
+            data.append(arg + (" " * (MAX - len(arg))))
+    print(" | ".join(data))
+    if FIRST:
+        print("_" * ((MAX + 3) * len(args)))
+
+
+def init():
+    if db.get_tables():
+        db.drop_tables([CLIENTS, ORDERS])
+    db.create_tables([CLIENTS, ORDERS])
+
+
+def fill():
+    for i in range(10):
+        order_data = orders_data[i].copy()
+        order_data["CLIENT"] = CLIENTS.create(**clients_data[i])
+        ORDERS.create(**order_data)
+
+
+def show(tablename):
+    table = {"orders": ORDERS, "clients": CLIENTS}[tablename]
+    pretty_print(*table._meta.sorted_field_names, FIRST=True)
+    for i in table.select():
+        # print(f"{i.CLIENT} | {i.DATE} | {i.AMOUNT} | {i.DESCRIPTION}")
+        pretty_print(
+            *tuple(str(getattr(i, field)) for field in table._meta.sorted_field_names)
+        )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(usage=argparse.SUPPRESS)
+    parser.add_argument("parametr", help="Параметр, что необходимо сделать, возможные значения: init, fill, show")
+    parser.add_argument("tablename", nargs="?", default="")
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        print("Использование: python лаба1.py init/fill/show [tablename]\n\ninit инициализирует базу данных\nfill заполняет базу данных случайными данными\nshow [tablename] показывает таблицу из базы данных")
+        quit(1)
+    parametr = args.parametr.lower()
+    tablename = args.tablename.lower()
+    if parametr not in ("init", "fill", "show"):
+        parser.error("Возможные значения parametr: init, fill, show")
+    if parametr == "show" and tablename not in ("orders", "clients"):
+        parser.error("show требует название таблицы, возможные значения: orders, clients")
+    args = [tablename] if tablename else []
+    db.connect()
+    try:
+        {"init": init, "fill": fill, "show": show}[parametr](*args)
+    finally:
+        db.close()
